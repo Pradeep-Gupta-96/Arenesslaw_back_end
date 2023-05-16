c3dda4e7e530ebf5df5e46cb8be7bc0f99221e17
Pradeep Kumar Gupta <120917855+Pradeep-Gupta-96@users.noreply.github.com>
2023-05-16 17:49:24 +0530

    code added

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -1,46 +1,45 @@
-import fs from 'fs'
-import { createTransport } from 'nodemailer'
-import Excel from '../models/excel.js'
-
-export const savehtml = async (req, res) => {
-    try {
-        const { html } = req.body
-        fs.writeFileSync("output.html", html)
-        return res.status(200).json({ message: "Saved" })
-    } catch (error) {
-        res.status(500).json({ message: error })
-    }
-}
+import { createTransport } from 'nodemailer';
+import Excel from '../models/excel.js';
 
 export const sendemail = async (req, res) => {
     try {
-        const id = (req.params.id)
-        await Excel.findByIdAndUpdate(id, req.body, {
+        const id = req.params.id;
+        const excelData = await Excel.findByIdAndUpdate(id, req.body, {
             new: true
-        })
-        // const transport = createTransport({
-        //     host: 'smtp.ethereal.email',
-        //     port: 587,
-        //     auth: {
-        //         user: 'abbigail14@ethereal.email',
-        //         pass: 'ujaWxheawN9rJdgv2R'
-        //     }
-        // });
+        });
 
-        // await transport.sendMail({
-        //     from: 'abbigail14@ethereal.email',
-        //     to: 'Pradeep@gmail.com',
-        //     subject: 'Notice Latter',
-        //     text: 'node js testing mail for areness',
-        //     attachments: [
-        //         {
-        //             // path: res.filename
-        //         }
-        //     ]
-        // });
+        const transport = createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'isadore.wiegand@ethereal.email',
+                pass: '3rjNKmrwDPXYtsN2AJ',
+            },
+        });
 
-        return res.status(200).json({ message: "Saved" })
+        const mailPromises = excelData.xlData.map(async (data) => {
+            const pdfUrl = `http://localhost:4000/excel/pdf/${id}/${data._id}`
+
+            const mailOptions = {
+                from: 'isadore.wiegand@ethereal.email',
+                to: data.E_mail,
+                subject: 'Notice Letter',
+                text: 'Node.js testing mail for awareness',
+                html: `<p>Click <a href="${pdfUrl}">here</a> to download the PDF file.</p>`,
+                attachments: [
+                    {
+                        filename: 'Notice.pdf',
+                        content: data.pdfBuffer,
+                    },
+                ],
+            };
+            await transport.sendMail(mailOptions);
+        });
+
+        await Promise.all(mailPromises);
+
+        return res.status(200).json({ message: "Saved" });
     } catch (error) {
-        res.status(500).json({ message: error })
+        res.status(500).json({ message: error });
     }
-}
+};
--- routes/noticeRoute.js
+++ routes/noticeRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { savehtml, sendemail } from "../controllers/noticeController.js";
+import { sendemail } from "../controllers/noticeController.js";
 export const noticeRouter=express.Router()
 
-noticeRouter.post('/savehtml',savehtml)
+
 noticeRouter.put('/sendemail/:id',sendemail)
