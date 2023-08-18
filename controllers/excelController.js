b97b5c86b81ac7e39ca2081e01c1340f1c7ffcfb
Pradeep Kumar Gupta <120917855+Pradeep-Gupta-96@users.noreply.github.com>
2023-08-18 13:07:46 +0530

    code added

--- controllers/excelController.js
+++ controllers/excelController.js
@@ -7,7 +7,6 @@ import bcrypt from 'bcrypt'; // Import bcrypt library
 export const postexceldata = async (req, res) => {
   try {
     const { filename, Bank, NoticeType } = req.body;
-    console.log(req.body)
     const userId = req.userId;
     const workbook = XLSX.readFile(req.file.path);
     const sheetNamelist = workbook.SheetNames;
@@ -31,9 +30,9 @@ export const postexceldata = async (req, res) => {
       const batch = updatedXlData.slice(i, i + batchSize);
       const batchPromises = batch.map(async (item) => {
         try {
-          const existingUser = await User.findOne({ username: item.SPOC_Email ? item.SPOC_Email.toLowerCase() : `User1234@example.com` });
+          const existingUser = await User.findOne({ email: item.FPR_NAME ? item.FPR_NAME.toLowerCase() : `User1234@example.com` });
           if (existingUser) {
-            console.log(`User with username ${item.SPOC_Email} already exists. Skipping...`);
+            // console.log(`User with username ${item.FPR_NAME} already exists. Skipping...`);
             return;
           }
           const hashpassword = await bcrypt.hash("Areness@123", 10);
@@ -69,61 +68,6 @@ export const postexceldata = async (req, res) => {
     res.status(500).json({ status: 500, success: false, msg: error.message });
   }
 };
-
-
-
-// export const postexceldata = async (req, res) => {
-//   try {
-//     const { filename, Bank } = req.body
-//     const userId = req.userId;
-//     const workbook = XLSX.readFile(req.file.path);
-//     const sheetNamelist = workbook.SheetNames;
-
-
-//     const processSheetData = async (sheetName) => {
-//       const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-//       const updatedXlData = await Promise.all(xlData.map(async (item) => {
-//         return item;
-//       }));
-//       return updatedXlData;
-//     };
-
-//     const updatedXlDataArrays = await Promise.all(sheetNamelist.map(processSheetData));
-//     const updatedXlData = [].concat(...updatedXlDataArrays);
-
-//     // Iterate through the xlData and create a new user for each item
-//     for (const item of updatedXlData) {
-//       try {
-//         const hashpassword = await bcrypt.hash("Areness@123", 10); // Hash the password
-//         const newUser = new User({
-//           username: item.SPOC_Email,
-//           email: item.SPOC_Email ? item.SPOC_Email.toLowerCase() : `User1234@example.com`, // Convert email to lowercase or generate a default email
-//           role: 'User',
-//           password: hashpassword, // Set a default password if undefined
-//           Bank: Bank
-//         });
-//         await newUser.save();
-//       } catch (error) {
-//         console.error('Error creating user:', error);
-//       }
-//     }
-
-//     await Excel.insertMany({
-//       filename,
-//       Bank,
-//       xlData: updatedXlData,
-//       userId,
-//     });
-
-
-//     // Delete the existing file   
-//     fs.unlinkSync(req.file.path);
-//     return res.json({ status: 200, success: true, msg: 'running' });
-//   } catch (error) {
-//     res.status(500).json({ status: 500, success: false, msg: error.message });
-//   }
-// };
-
 
 
 export const getAllexceldata = async (req, res) => {
